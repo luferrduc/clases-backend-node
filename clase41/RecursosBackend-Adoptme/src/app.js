bf97140ccbb4894ea75fd706b367467a3e6647fc
luferrduc <lu.ferrando@duocuc.cl>
2024-02-15 21:09:03 -0300

    After class 15-02. Autogen swagger. En la clase 41, RecursosBackend-Adoptme

--- clase41/RecursosBackend-Adoptme/src/app.js
+++ clase41/RecursosBackend-Adoptme/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+import swaggerUiExpress from 'swagger-ui-express';
 
 import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
@@ -9,12 +10,16 @@ import sessionsRouter from './routes/sessions.router.js';
 import __mainDirname from './utils/index.js';
 
 
+import swaggerFile from '../swagger-output.json' assert { type: 'json' }
+
 const app = express();
 const PORT = process.env.PORT||8080;
 const connection = mongoose.connect('mongodb+srv://lucianoferrando94:RxBWSWiQeU9Ic0Xy@cluster55575.ixldvmd.mongodb.net/clase41?retryWrites=true&w=majority')
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use('/api/docs', swaggerUiExpress.serve, swaggerUiExpress.setup(swaggerFile))
 
 app.use('/api/users',usersRouter);
 app.use('/api/pets',petsRouter);
