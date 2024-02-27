54619db61a6f0d431848816640a512137050f9ab
luferrduc <lu.ferrando@duocuc.cl>
2024-02-26 21:42:14 -0300

    Crear directorios de backend y frontend

---
+++ clase46/backend/routes/payment.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+
+const router = Router()
+
+const products = [
+  { id: 1, name: "papas", price: 1000 },
+  { id: 2, name: "queso", price: 500 },
+  { id: 3, name: "hamburguesa", price: 1500 },
+  { id: 4, name: "soda", price: 1000 },
+  { id: 5, name: "golosinas", price: 800 }
+]
+
+router.post("/payment-intents", async (req, res) => {
+  
+})
+
+
+
+export default router
